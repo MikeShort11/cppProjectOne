4e67581ebe0ee218f3f25a52e9fd6ff89537ceea
Michael <michael.short094@gmail.com>
2024-09-11 11:05:30 -0600

    add basic function

--- projectOne.cpp
+++ projectOne.cpp
@@ -4,7 +4,22 @@ using namespace std;
 
 int main(){
 
+	int playerNums[5];
+	int playerRtings[5];
 
+	for (int i = 1; i < 6; ++i){
+		cout << "enter player" << i << "'s jersey number:" << endl;
+		cin >> playerNums[i];
+		cout << "enter player" << i << "'s rating:" << endl;
+		cin >> playerRtings[i];	
+	}
 
+	cout << "ROSTER" << endl;
+
+	for (int i = 1; i < 6; ++i){
+
+		cout << "Player 1 -- Jersey number: " << playerNums[i] << " Rating: " <<playerRtings[i] << endl;
+
+	}
 return 0;
 }
