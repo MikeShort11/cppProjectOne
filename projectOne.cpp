9b7ec8110ef9b07e80f8e82e1f5bcc9fa85a3de8
Michael <michael.short094@gmail.com>
2024-09-11 13:04:45 -0600

    add menu and quit/show roster functionality

--- projectOne.cpp
+++ projectOne.cpp
@@ -1,25 +1,56 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main(){
 
-	int playerNums[5];
-	int playerRtings[5];
+	vector<int> playerNums;
+	vector<int> playerRtngs;
 
+	//get inital values fro jerseys and ratings
 	for (int i = 1; i < 6; ++i){
+		int temp;
 		cout << "enter player" << i << "'s jersey number:" << endl;
-		cin >> playerNums[i];
+		cin >> temp;
+		playerNums.push_back(temp);
 		cout << "enter player" << i << "'s rating:" << endl;
-		cin >> playerRtings[i];	
+		cin >> temp;
+		playerRtngs.push_back(temp);	
 	}
 
-	cout << "ROSTER" << endl;
+	//show roster after setting intital values
+	cout << endl << "ROSTER" << endl;
 
-	for (int i = 1; i < 6; ++i){
+	for (int i = 0; i < 5; ++i){
 
-		cout << "Player 1 -- Jersey number: " << playerNums[i] << " Rating: " <<playerRtings[i] << endl;
+		cout << "Player 1 -- Jersey number: " << playerNums.at(i) << " Rating: " <<playerRtngs.at(i) << endl;
+	}
+
+	//menu loop
+	while (true) {
+		// note: loop and after any selection	
+		char input;
+
+		// menu text
+		cout << "MENU" << endl << "a - Add player" << endl << "d - Remove player" << endl << "u - Update player rating" << endl << "r - Output players above a rating" << endl << "o - Output roster" << endl << "q - Quit" << endl << endl << "Choose an option:" << endl;
+		cin >> input;
+		
+		// in input is q end the loop
+		if (input == 'q' || input == 'Q') {
+			cout << "ending";
+			break;
+		// show the roster if the input is o
+		} else if (input == 'o' || input == 'O'){
+			cout << "ROSTER" << endl;
+			for (int i = 0; i < 5; ++i){
+				cout << "Player 1 -- Jersey number: " << playerNums.at(i) << " Rating: " <<playerRtngs.at(i) << endl;
+		}
+
+		}
+
+	
+		return 0;
 
 	}
-return 0;
 }
